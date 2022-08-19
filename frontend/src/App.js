ea14b6dc115a2a66a09cdf681435c9eebcba339c
최정원/백화점1팀/173917 <173917@ssg.global>
2022-08-19 09:36:54 +0900

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import 'App.scss';
+import { Button } from "antd"
+
+function App() {
+  return (
+    <div>
+      Hello, React
+      <Button type='primary'>Hi</Button>
+    </div>
+  );
+}
+
+export default App;
